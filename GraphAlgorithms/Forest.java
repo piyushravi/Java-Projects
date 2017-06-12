fc1d44dcbb4c721dc1a447810c683ed578ea5f4f
Piyush Ravi <piyush.ravi_ug19@ashoka.edu.in>
2017-06-12 14:14:01 +0530

    Add files via upload

---
+++ GraphAlgorithms/Forest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphalgorithms;
+
+/**
+ *
+ * @author piyush
+ */
+public class Forest {
+    private Tree [] trees;
+    
+    Forest(Tree []trees){
+        this.trees=trees;
+    }
+    
+}
---
+++ GraphAlgorithms/Tree.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphalgorithms;
+
+/**
+ *
+ * @author piyush
+ */
+public class Tree {
+    private Vertex root;
+    private Edge [] edges;
+        
+    Tree(Vertex root){
+        this.root=root;
+        this.edges=[];
+    }
+    
+}
