1dde73f8ade777ba45f5037058f9a9d6e9316b12
Piyush Ravi <piyush.ravi_ug19@ashoka.edu.in>
2017-06-12 16:27:37 +0530

    Add files via upload

---
+++ GraphAlgorithms/Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphalgorithms;
+
+/**
+ *
+ * @author piyush
+ */
+public class Node {
+    private Node parent;
+    private Node [] children;
+    Node(){
+    Node.parent=null;
+    Node.children=null;
+    }
+    
+}
