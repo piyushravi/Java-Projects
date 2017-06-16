46e974649b0aa727a5dd83edd04f808239863c0b
Piyush Ravi <piyush.ravi_ug19@ashoka.edu.in>
2017-06-16 16:29:22 +0530

    Update Graph.java

--- GraphAlgorithms/Graph.java
+++ GraphAlgorithms/Graph.java
@@ -9,16 +9,41 @@ package graphalgorithms;
  *
  * @author piyush
  */
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
         
 public class Graph {
     private List<Vertex> vertices= new ArrayList<>();
     private List<Edge> edges= new ArrayList<>();
+    private Map adjMat= new HashMap();
+    private boolean madeAdjacencyMatrix= false;
     
     Graph(ArrayList <Vertex> vertices, ArrayList <Edge> edges){
         this.vertices=vertices;
         this.edges=edges;
+    }
+    
+    public void addVertex(Vertex vertex){
+        this.vertices.add(vertex);
+    }
+    
+    public void addEdge(Edge edge){
+        this.edges.add(edge);
+        if (this.madeAdjacencyMatrix)
+    }
+    
+    public void makeAdjacencyMatrix(){
+        this.madeAdjacencyMatrix=true;
+        
+    
+    }
+    
+    public Map getAdjacencyMatrix(){
+        if (madeAdjacencyMatrix==False){
+            this.makeAdjacencyMatrix();
+            
+        }
+        
+        return this.adjMat;
     }
     
 }
